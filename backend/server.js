893ec795ae2b36bab3f25d5df1f14fdc556e1d33
bumair97@gmail.com <bumair97@gmail.com>
2023-01-01 21:30:35 +0500

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Task = require("./models/taskModel");
+const taskRoute = require("./routes/taskRoute");
+const cors = require("cors")
+
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use("/api/tasks", taskRoute);
+
+// Routes
+app.get("/", (req, res) => {
+    res.send('Home Page')
+});
+
+mongoose.set('strictQuery', false);
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() =>{
+    app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });   
+  }).catch((err) => console.log(err));        
+                 
